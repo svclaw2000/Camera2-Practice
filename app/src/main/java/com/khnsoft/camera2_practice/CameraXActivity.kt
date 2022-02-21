191a7f54fdabcc3d0a99b61c27c7a51c56407b5f
Keich.Park <svclaw2000@gmail.com>
2022-02-21 21:36:35 +0900

    feat: CameraX 프리뷰 추가

--- app/src/main/java/com/khnsoft/camera2_practice/CameraXActivity.kt
+++ app/src/main/java/com/khnsoft/camera2_practice/CameraXActivity.kt
@@ -2,15 +2,40 @@ package com.khnsoft.camera2_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.ListenableFuture
 import com.khnsoft.camera2_practice.databinding.ActivityCameraXBinding
 
 class CameraXActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCameraXBinding
 
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraXBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
+        val preview = Preview.Builder()
+            .build()
+        val cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+            .build()
+
+        preview.setSurfaceProvider(binding.previewView.surfaceProvider)
+        val camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview)
     }
 }
