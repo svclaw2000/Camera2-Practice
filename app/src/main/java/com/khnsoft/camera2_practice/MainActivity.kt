f657dad3762bc1fb41c1eb22094171ffdd446b15
Keich.Park <svclaw2000@gmail.com>
2022-02-21 19:05:05 +0900

    feat: 카메라 권한 요청

--- app/src/main/java/com/khnsoft/camera2_practice/MainActivity.kt
+++ app/src/main/java/com/khnsoft/camera2_practice/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.khnsoft.camera2_practice
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (!isGranted) {
+                Toast.makeText(
+                    this,
+                    "카메라 권한을 승인해주셔야 어플을 사용할 수 있습니다.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startRequestPermission()
+    }
+
+    private fun startRequestPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+        }
     }
 }
